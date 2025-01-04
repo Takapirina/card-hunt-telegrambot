59d0a9449e4e32da93faf3115c8a4076c004ab20
Andriy <90765360+Takapirina@users.noreply.github.com>
2025-01-04 16:03:15 +0100

    update minor fix?

--- service/seleniumService.py
+++ service/seleniumService.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
 
 class SeleniumService:
     _instance = None
@@ -36,77 +36,28 @@ class SeleniumService:
             raise RuntimeError("Il driver Selenium non è stato inizializzato correttamente.")
         try:
             self.driver.get(url)
-
             WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, "img.is-front"))
-            )
-
-            tabella = WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, ".table.article-table.table-striped"))
-            )
+                EC.presence_of_element_located((By.CSS_SELECTOR, "img.is-front"))
+            )  # Attendere che l'immagine con classe 'is-front' sia presente
+            print("Pagina caricata correttamente.")
+            
+            tabella = self.driver.find_element(By.CSS_SELECTOR, ".table.article-table.table-striped")
             listaPrezzi = tabella.find_elements(By.CSS_SELECTOR, 
                 "div.price-container.d-none.d-md-flex.justify-content-end span.color-primary.small.text-end.text-nowrap.fw-bold")
-            return float(listaPrezzi[0].text.replace("€", "").replace(",", ".").strip())
-        except NoSuchElementException:
-            print("Elemento richiesto non trovato sulla pagina.")
+            
+            if listaPrezzi:
+                return float(listaPrezzi[0].text.replace("€", "").replace(",", ".").strip())
+            else:
+                print("Nessun prezzo trovato.")
+                return None
+        except TimeoutException:
+            print("Tempo di attesa scaduto per caricare l'elemento.")
+            return None
+        except NoSuchElementException as e:
+            print(f"Elemento richiesto non trovato sulla pagina: {e}")
             return None
         except Exception as e:
             print(f"Errore durante la validazione dell'URL: {e}")
-            return None
-
-    def get_prize(self, url):
-        """Ottiene il prezzo dalla pagina data."""
-        if not self.driver:
-            raise RuntimeError("Il driver Selenium non è stato inizializzato correttamente.")
-        try:
-            self.driver.get(url)
-
-            WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, "img.is-front"))
-            )
-
-            tabella = WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, ".table.article-table.table-striped"))
-            )
-            listaPrezzi = tabella.find_elements(By.CSS_SELECTOR, 
-                "div.price-container.d-none.d-md-flex.justify-content-end span.color-primary.small.text-end.text-nowrap.fw-bold")
-            return float(listaPrezzi[0].text.replace("€", "").replace(",", ".").strip())
-        except NoSuchElementException:
-            print("Elemento richiesto non trovato sulla pagina.")
-            return None
-        except Exception as e:
-            print(f"Errore durante il recupero del prezzo: {e}")
-            return None
-        
-    def update_prize(self, url):
-        """Aggiorna il prezzo sulla pagina."""
-        try:
-            self.driver.get(url)
-
-            WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, "div.info-list-container dl.labeled dd"))
-            )
-            lista_dati = self.driver.find_elements(By.CSS_SELECTOR, "div.info-list-container dl.labeled dd")
-            if len(lista_dati) >= 6 and len(lista_dati) <= 10:
-                prezzo_corrente = float(lista_dati[6].text.replace("€","").replace(",","."))
-            else:
-                prezzo_corrente = float(lista_dati[7].text.replace("€","").replace(",","."))
-
-            tabella = WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, ".table.article-table.table-striped"))
-            )
-            listaPrezzi = tabella.find_elements(By.CSS_SELECTOR, 
-                "div.price-container.d-none.d-md-flex.justify-content-end span.color-primary.small.text-end.text-nowrap.fw-bold")
-            prezzo_attuale =  float(listaPrezzi[0].text.replace("€", "").replace(",", ".").strip())
-            return {
-                "prezzo_di_tendenza": prezzo_corrente,
-                "prezzo_attuale" : prezzo_attuale
-            }
-        except NoSuchElementException:
-            print("Elemento richiesto non trovato sulla pagina.")
-            return None
-        except Exception as e:
-            print(f"Errore durante il recupero del prezzo: {e}")
             return None
 
     def quit(self):
