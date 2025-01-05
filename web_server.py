b3fef61a82dd0fa425a5b17db078bd063ffd9da7
Andriy <90765360+Takapirina@users.noreply.github.com>
2025-01-05 16:31:06 +0100

    update web service...

--- web_server.py
+++ web_server.py
@@ -1,12 +1,15 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/callback", methods=["GET"])
-def handle_callback():
-    code = request.args.get("code")
-    if code:
-        print(f"Codice ricevuto: {code}")
+@app.route('/callback', methods=['GET'])
+def callback():
+    code = request.args.get('code')
+    if not code:
+        return "Code not provided", 400
+    return f"Received code: {code}", 200
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
