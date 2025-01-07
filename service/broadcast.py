16e22658d412c88a6b2ee5b49dc4017da6828142
Andriy <90765360+Takapirina@users.noreply.github.com>
2025-01-07 19:19:53 +0100

    aggiunta logica di rilancio scraping...

--- service/broadcast.py
+++ service/broadcast.py
@@ -24,64 +24,81 @@ async def invia_messaggio_utenti(context: ContextTypes.DEFAULT_TYPE):
             for carta in wishList["carte"]:
                 url = carta["url_card"]
                 print(carta['nome_personalizzato'])
-                data = selenium.update_prize(url)
-                dizionari.append(data)
-                user.aggiorna_carta_prezzo(carta["id_carta"], data["prezzo_attuale"])
-
-                flags = ['🇬🇧', '🇫🇷', '🇩🇪', '🇪🇸', '🇮🇹', '🇨🇳', '🇯🇵', '🇵🇹', '🏳️', '🇰🇷', '🇹🇼']
                 
-                percentuale_di_variazione = round(((data["prezzo_attuale"] - carta["prezzo_attuale"]) / carta["prezzo_attuale"]) * 100,2)
-                if percentuale_di_variazione < 0:
-                    stringa_di_variazione = f"[{percentuale_di_variazione}%🔽]"
-                elif percentuale_di_variazione > 0:
-                    stringa_di_variazione = f"[+{percentuale_di_variazione}%🔼]"
+                tentativi = 0
+                max_tentativi = 3
+                data = None
+
+                while tentativi < max_tentativi:
+                    try:
+                        data = selenium.update_prize(url)
+                        break 
+                    except Exception as e:
+                        tentativi += 1
+                        print(f"Errore durante il tentativo di aggiornamento per {carta['nome_personalizzato']} (Tentativo {tentativi}): {e}")
+                        if tentativi >= max_tentativi:
+                            print(f"Massimo numero di tentativi raggiunto per {carta['nome_personalizzato']}")
+
+                if data:
+                    dizionari.append(data)
+                    user.aggiorna_carta_prezzo(carta["id_carta"], data["prezzo_attuale"])
+
+                    flags = ['🇬🇧', '🇫🇷', '🇩🇪', '🇪🇸', '🇮🇹', '🇨🇳', '🇯🇵', '🇵🇹', '🏳️', '🇰🇷', '🇹🇼']
+                    
+                    percentuale_di_variazione = round(((data["prezzo_attuale"] - carta["prezzo_attuale"]) / carta["prezzo_attuale"]) * 100, 2)
+                    if percentuale_di_variazione < 0:
+                        stringa_di_variazione = f"[{percentuale_di_variazione}%🔽]"
+                    elif percentuale_di_variazione > 0:
+                        stringa_di_variazione = f"[+{percentuale_di_variazione}%🔼]"
+                    else:
+                        stringa_di_variazione = ""
+
+                    percentuale_di_variazione_tendenza = round(((data["prezzo_di_tendenza"] - data["prezzo_attuale"]) / data["prezzo_attuale"]) * 100, 2)
+                    if percentuale_di_variazione_tendenza < 0:
+                        stringa_di_variazione_tendenza = f"[{percentuale_di_variazione_tendenza}%🔽]"
+                    elif percentuale_di_variazione_tendenza > 0:
+                        stringa_di_variazione_tendenza = f"[+{percentuale_di_variazione_tendenza}%🔼]"
+                    else:
+                        stringa_di_variazione_tendenza = ""
+
+                    prezzo_settimanale = carta["prezzo_settimanale"]
+                    percentuale_di_variazione_settimanale = round(((data["prezzo_attuale"] - prezzo_settimanale) / prezzo_settimanale) * 100, 2)
+                    if percentuale_di_variazione_settimanale < 0:
+                        stringa_di_variazione_settimanale = f"[{percentuale_di_variazione_settimanale}%🔽 Settimanale]"
+                    elif percentuale_di_variazione_settimanale > 0:
+                        stringa_di_variazione_settimanale = f"[+{percentuale_di_variazione_settimanale}%🔼 Settimanale]"
+                    else:
+                        stringa_di_variazione_settimanale = f"[0% Settimanale]"
+
+                    prezzo_mensile = carta["prezzi_mensili"][-1] 
+                    percentuale_di_variazione_mensile = round(((data["prezzo_attuale"] - prezzo_mensile) / prezzo_mensile) * 100, 2)
+                    if percentuale_di_variazione_mensile < 0:
+                        stringa_di_variazione_mensile = f"[{percentuale_di_variazione_mensile}%🔽 Mensile]"
+                    elif percentuale_di_variazione_mensile > 0:
+                        stringa_di_variazione_mensile = f"[+{percentuale_di_variazione_mensile}%🔼 Mensile]"
+                    else:
+                        stringa_di_variazione_mensile = f"[0% Mensile]"
+
+                    
+                    nota = ""
+                    if percentuale_di_variazione_settimanale > 10:
+                        nota += "⚡️ Il prezzo settimanale è aumentato notevolmente! "
+                    if percentuale_di_variazione_settimanale < -10:
+                        nota += "📉 Il prezzo settimanale è diminuito drasticamente. Potrebbe essere il momento giusto per acquistare. "
+                    if data["prezzo_attuale"] > data["prezzo_di_tendenza"] * 1.5:
+                        nota += "⚠️ <b>Attenzione!</b> Il prezzo attuale è molto più alto della tendenza, potrebbe essere manipolato. "
+                    if data["prezzo_attuale"] < data["prezzo_di_tendenza"] * 0.5:
+                        nota += "⚠️ <b>Attenzione!</b> Il prezzo attuale è molto più basso della tendenza, potrebbe esserci una svalutazione artificiale. "
+                    if nota == "":
+                        nota = "⏳ Il prezzo è stabile, nessun cambiamento significativo."
+
+                    stringa = f"🗂️<a href='{carta['url_card']}'>{carta['nome_personalizzato']}</a> <b>({carta['codice_espansione']} {carta['numero_carta']})</b> {flags[int(carta['lingua_carta'])-1]} [{carta['prezzo_iniziale']}€]\n"
+                    stringa += f"<b>Prezzo attuale</b>: {carta['prezzo_attuale']}€ {stringa_di_variazione} | <b>Prezzo di tendenza</b>: {data['prezzo_di_tendenza']}€ {stringa_di_variazione_tendenza}\n"
+                    stringa += f"<b>Variazione Settimanale</b>: {stringa_di_variazione_settimanale} | <b>Variazione Mensile</b>: {stringa_di_variazione_mensile}\n"
+                    stringa += f"{nota}\n"
+                    carte_recap.append(stringa)
                 else:
-                    stringa_di_variazione = ""
-
-                percentuale_di_variazione_tendenza = round(((data["prezzo_di_tendenza"] - data["prezzo_attuale"]) / data["prezzo_attuale"]) * 100,2)
-                if percentuale_di_variazione_tendenza < 0:
-                    stringa_di_variazione_tendenza = f"[{percentuale_di_variazione_tendenza}%🔽]"
-                elif percentuale_di_variazione_tendenza > 0:
-                    stringa_di_variazione_tendenza = f"[+{percentuale_di_variazione_tendenza}%🔼]"
-                else:
-                    stringa_di_variazione_tendenza = ""
-
-                prezzo_settimanale = carta["prezzo_settimanale"]
-                percentuale_di_variazione_settimanale = round(((data["prezzo_attuale"] - prezzo_settimanale) / prezzo_settimanale) * 100, 2)
-                if percentuale_di_variazione_settimanale < 0:
-                    stringa_di_variazione_settimanale = f"[{percentuale_di_variazione_settimanale}%🔽 Settimanale]"
-                elif percentuale_di_variazione_settimanale > 0:
-                    stringa_di_variazione_settimanale = f"[+{percentuale_di_variazione_settimanale}%🔼 Settimanale]"
-                else:
-                    stringa_di_variazione_settimanale = f"[0% Settimanale]"
-
-                prezzo_mensile = carta["prezzi_mensili"][-1] 
-                percentuale_di_variazione_mensile = round(((data["prezzo_attuale"] - prezzo_mensile) / prezzo_mensile) * 100, 2)
-                if percentuale_di_variazione_mensile < 0:
-                    stringa_di_variazione_mensile = f"[{percentuale_di_variazione_mensile}%🔽 Mensile]"
-                elif percentuale_di_variazione_mensile > 0:
-                    stringa_di_variazione_mensile = f"[+{percentuale_di_variazione_mensile}%🔼 Mensile]"
-                else:
-                    stringa_di_variazione_mensile = f"[0% Mensile]"
-
-                
-                nota = ""
-                if percentuale_di_variazione_settimanale > 10:
-                    nota += "⚡️ Il prezzo settimanale è aumentato notevolmente! "
-                if percentuale_di_variazione_settimanale < -10:
-                    nota += "📉 Il prezzo settimanale è diminuito drasticamente. Potrebbe essere il momento giusto per acquistare. "
-                if data["prezzo_attuale"] > data["prezzo_di_tendenza"] * 1.5:
-                    nota += "⚠️ <b>Attenzione!</b> Il prezzo attuale è molto più alto della tendenza, potrebbe essere manipolato. "
-                if data["prezzo_attuale"] < data["prezzo_di_tendenza"] * 0.5:
-                    nota += "⚠️ <b>Attenzione!</b> Il prezzo attuale è molto più basso della tendenza, potrebbe esserci una svalutazione artificiale. "
-                if nota == "":
-                    nota = "⏳ Il prezzo è stabile, nessun cambiamento significativo."
-
-                stringa = f"🗂️<a href='{carta['url_card']}'>{carta['nome_personalizzato']}</a> <b>({carta['codice_espansione']} {carta['numero_carta']})</b> {flags[int(carta['lingua_carta'])-1]} [{carta['prezzo_iniziale']}€]\n"
-                stringa += f"<b>Prezzo attuale</b>: {carta['prezzo_attuale']}€ {stringa_di_variazione} | <b>Prezzo di tendenza</b>: {data['prezzo_di_tendenza']}€ {stringa_di_variazione_tendenza}\n"
-                stringa += f"<b>Variazione Settimanale</b>: {stringa_di_variazione_settimanale} | <b>Variazione Mensile</b>: {stringa_di_variazione_mensile}\n"
-                stringa += f"{nota}\n"
-                carte_recap.append(stringa)
+                    print(f"Errore nell'aggiornamento dei dati per {carta['nome_personalizzato']}")
 
         finally:
             selenium.quit()
